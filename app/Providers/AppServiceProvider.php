462ceb54dfc1e980d382dd3c0b94e8b2819fa5a8
Shawn Weigand <shawnweigand1@gmail.com>
2025-09-09 08:32:13 -0400

    Update dependencies in composer.json and composer.lock, configure Vite server settings, and enhance AppServiceProvider for production URL scheme and action commands registration.

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -2,7 +2,9 @@
 
 namespace App\Providers;
 
+use Illuminate\Support\Facades\URL;
 use Illuminate\Support\ServiceProvider;
+use Lorisleiva\Actions\Facades\Actions;
 
 class AppServiceProvider extends ServiceProvider
 {
@@ -19,6 +21,10 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
-        //
+        if (config('app.env') === 'production') {
+            URL::forceScheme('https');
+        }
+
+        Actions::registerCommands();
     }
 }
